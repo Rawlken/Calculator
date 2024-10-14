d499c8cd610dbb53242341788359168579854a03
Rawlken <117388813+Rawlken@users.noreply.github.com>
2024-10-14 20:31:48 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,23 +1,45 @@
 print('''----------------------------------------------------------------------------------
 Welcome To The Calculator!
 ----------------------------------------------------------------------------------''')
+
 while True:
-    number1 = float(input("Enter A Number: "))
-    number2 = float(input("Enter A Number: "))
-    transaction = input("For Addition (+) For Extraction (-) For Multiplication (*) For Partition (/) Enter: ")
-    if (transaction == "+" ):
-        total = number1 + number2
-        print('Sum Of Two Numbers: ',total)
-    elif (transaction == "-"):
-        extraction = number1 - number2
-        print("Difference Of Two Numbers: ",extraction)
-    elif (transaction == "*"):
-        multiplication = number1 * number2
-        print("Multiplication Of Two Numbers: ",multiplication)
-    elif (transaction == "/"):
-        result = number1 / number2 
-        print("Division Of Two Numbers: ",result)
-    else:
+    try:
+        number1 = float(input("Enter A Number: "))
+    except ValueError:
         print("You Enter An Invalid Result")
-        exit()
+        print('''----------------------------------------------------------------------------------''')
+        continue
+    try:
+        number2 = float(input("Enter A Number: "))
+    except ValueError:
+        print("You Enter An Invalid Result")
+        print('''----------------------------------------------------------------------------------''')
+        continue
+    transaction = input("For Addition (+) For Extraction (-) For Multiplication (*) For Partition (/) Enter: ")
+
+    if (transaction != "+" and transaction != "-" and transaction != "*" and transaction != "/"):
+        print("You Enter An Invalid Result")
+        
+    else:
+        if (transaction == "+" ):
+            total = number1 + number2
+            print('Sum Of Two Numbers: ', total)
+        elif (transaction == "-"):
+            extraction = number1 - number2
+            print("Difference Of Two Numbers: ", extraction)
+        elif (transaction == "*"):
+            multiplication = number1 * number2
+            print("Multiplication Of Two Numbers: ", multiplication)
+        elif (transaction == "/"):
+            if (number2 == 0):
+                print("You Can't Divide By Zero")
+                print('''----------------------------------------------------------------------------------''')
+                continue
+            result = number1 / number2 
+            print("Division Of Two Numbers: ", result)
+        else:
+            print("You Enter An Invalid Result")
+            exit()
+    
+
     print('''----------------------------------------------------------------------------------''')
